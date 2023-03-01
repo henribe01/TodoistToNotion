039cfb81757d308ea343bde485b5c72d0046eed8
Cookieman01 <13785517+Cookieman01@users.noreply.github.com>
2023-03-01 16:46:58 +0100

    reworked notion.py

--- notion.py
+++ notion.py
@@ -1,81 +1,78 @@
 import requests
 import os
+
 from dotenv import load_dotenv
 
 load_dotenv()
-api_url = 'https://api.notion.com/v1/'
-
-headers = {'Authorization': f'Bearer {os.getenv("NOTION_API_KEY")}',
-           'accept': 'application/json',
-           'Notion-Version': '2022-06-28'}
-
-data = {"parent": {'type': 'database_id',
-                   "database_id": os.getenv('NOTION_DB_ID')}}
 
 
 class Notion:
-    # TODO: Add function to update page
-    # TODO: Add function to delete page
-    # TODO: Add error handling
+    api_url = 'https://api.notion.com/v1/'
+    headers = {'Authorization': f'Bearer {os.getenv("NOTION_API_KEY")}',
+               'accept': 'application/json',
+               'Notion-Version': '2022-06-28'}
+
     def __init__(self, database_id):
         self.database_id = database_id
         self.database = self.get_database()
+        self.data = {"parent": {'type': 'database_id',
+                                "database_id": database_id}}
 
     def get_database(self):
-        return requests.get(api_url + 'databases/' + self.database_id, headers=headers).json()
+        return requests.get(f'{Notion.api_url}databases/{self.database_id}', headers=Notion.headers).json()
 
     def create_property(self, **kwargs):
         """
-        Creates a property object
-        :param kwargs: Should be of form "name": "value"
-        :return: A dictonary of properties
+        Creates a dict in the format of properties for the Notion API
+        :param kwargs: Format 'NotionColumnName=Value'
+        :return: Properties Dict
         """
-        # Makes a copy of the properties of the database if it is in kwargs
-        properties = {key: value.copy() for key, value in self.database['properties'].items() if key in kwargs}
-        for key, value in properties.items():
-            # Deletes the name because notion doesn't like it
-            del properties[key]['name']
-            type = properties[key]['type']
-            if type in ['select', 'multi_select']:
-                # Handles the select and multi_select types
-                selected_option = [option for option in properties[key][type]['options'] if
-                                   option['name'] == kwargs[key] or option['name'] in kwargs[key]]
-                if type == 'select':
-                    properties[key][type] = selected_option[0]
-                elif type == 'multi_select':
-                    properties[key][type] = selected_option
-            elif type == 'title':
-                properties[key][type] = [{'text': {'content': kwargs[key]}}]
-            elif type == 'date':
-                properties[key][type] = {'start': kwargs[key]}
-            elif type == 'rich_text':
-                properties[key][type] = [{'type': 'text', 'text': {'content': kwargs[key]}}]
-            else:
-                properties[key][type] = kwargs[key]
+        properties = {}
+        for name, value in kwargs.items():
+            # Check if the Column is in the database
+            if name not in self.database['properties'].keys():
+                # TODO: Add logging
+                pass
+            prop_type = self.database['properties'][name]['type']
+            property = {'type': prop_type, prop_type: None}
+            match prop_type:
+                case "checkbox":
+                    if type(value) != bool:
+                        # TODO: add Error
+                        pass
+                    property[prop_type] = value
+                case "date":
+                    property[prop_type] = {'start': value}
+                case "multi_select":
+                    property[prop_type] = [{'name': tag} for tag in value]
+                case "number":
+                    property[prop_type] = value
+                case "select":
+                    property[prop_type] = {'name': value}
+                case "title":
+                    property[prop_type] = [{'type': 'text', 'text': {'content': value}}]
+                case "rich_text":
+                    property[prop_type] = [{'type': 'text', 'text': {'content': value}}]
+                # TODO: Add case for relation
+            properties[name] = property
         return properties
 
     def create_page(self, properties):
         """
-        Creates a page in the database
-        :return: The response from the API
+        Creates a page in the Database
+        :param properties: Dictonary of the properties
+        :return: Response from the API
         """
+        data = self.data
         data['properties'] = properties
-        return requests.post(api_url + 'pages', headers=headers, json=data).json()
+        return requests.post(f'{Notion.api_url}pages', headers=Notion.headers, json=data).json()
 
-    def get_pages(self, **kwargs):
-        """
-        Gets all the pages from the database
-        :param kwargs: filters (name of property: condition(dict))
-        :return: A list of pages
-        """
-        # TODO: Change so that it can handle multiple filters
-        filters = {'and': [], 'or': []}
-        for key, value in kwargs.items():
-            type = self.database['properties'][key]['type']
-            filters['and'].append({'property': key, type: value})
-        return requests.post(api_url + f'databases/{self.database_id}/query', headers=headers,
-                             json={'filter': filters}).json()['results']
+    def get_pages(self):
+        # TODO: Add filter
+        return requests.post(f'{Notion.api_url}databases/{self.database_id}/query', headers=Notion.headers).json()
 
-# notion = Notion(os.getenv('NOTION_DB_ID'))
-# property = notion.create_property(Name='Test', Datum='2021-12-31', Fach='Numerik', Tags=['Uni', 'Heute'])
-# print(notion.get_pages(Tags={'contains': 'Heute'}))
+
+notion = Notion(os.getenv('NOTION_DB_ID'))
+properties = notion.create_property(Name='test', Datum='2023-02-24', Tags=['Uni', 'test123'],
+                                    TodoistID='8123887asdj')
+print(notion.get_pages()['results'][0])
